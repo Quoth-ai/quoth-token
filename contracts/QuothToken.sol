0ccf4bd3b572637be1dd620f16f52bd2fc7e045a
DeFi Jesse <jesse@gordian.agency>
2022-02-21 16:01:26 +0100

    Update solidity version and token constructor minting

--- contracts/QuothToken.sol
+++ contracts/QuothToken.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.11;
+pragma solidity 0.8.12;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -18,7 +18,7 @@ contract QuothToken is ERC20, Ownable {
     uint256 amount_
   ) ERC20(name_, symbol_) {
     require(amount_ > 0, "QuothToken: Amount is zero");
-    _mint(recipient_, amount_);
+    _mint(recipient_, amount_ * 10 ** decimals());
   }
 
   function setBP(address _bp) external onlyOwner {
